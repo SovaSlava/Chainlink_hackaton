3a1f90374c143f8902b16352fc99b9c7cc3e72ad
Viacheslav <slavahustle@gmail.com>
2022-11-09 03:01:26 +0300

    object -> string

--- externalAdapter/app.ts
+++ externalAdapter/app.ts
@@ -1,24 +1,26 @@
-import  { createRequest } from './index.js'
+import { createRequest } from './index.js'
 import express from "express";
-import  {  Express, Request, Response} from 'express';
-const app:Express = express()
+import { Express, Request, Response } from 'express';
+const app: Express = express()
 const port = process.env.EA_PORT || 8080
 type callbackType = {
-  jobRunID:string,
-  data:{ result: string | string[] | boolean,
-         requestStatus: number
+  jobRunID: string,
+  data: {
+    result: string | string[] | boolean,
+    requestStatus: number
   }
 }
 
 app.use(express.json())
 
-app.post('/', (req:Request, res:Response) => {
-  console.log(`Request data: ${req.body}`)
- 
-  createRequest(req.body, (callbackData:callbackType) => {
+app.post('/', (req: Request, res: Response) => {
+  console.log('New request')
+
+  createRequest(req.body, (callbackData: callbackType) => {
+    console.log('Send response')
     res.status(200).json(callbackData)
   })
-  
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
--- externalAdapter/index.ts
+++ externalAdapter/index.ts
@@ -1,24 +1,25 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
-const customError = (data:string) => {
+const customError = (data: string) => {
   return false
 }
 type callbackType = {
-  jobRunID:string,
-  data:{ result: string | string[] | boolean,
-         requestStatus: number
+  jobRunID: string,
+  data: {
+    result: string | string[] | boolean,
+    requestStatus: number
   }
 }
-const CustomParams =  {
-  "extract": { 
+const CustomParams = {
+  "extract": {
     url: true,
     regexp: true,
     matchIndex: true
   },
-  "match": { 
+  "match": {
     url: true,
     regexp: true
   },
-  "multiExtract": { 
+  "multiExtract": {
     url: true,
     regexp: true
   }
@@ -36,97 +37,91 @@ type defaultArgsTypes = {
 };
 const defaultArgs: defaultArgsTypes = {
   "match": false,
-  "extract":"",
-  "multiExtract":[]
+  "extract": "",
+  "multiExtract": []
 };
 type jobTypeArg = "match" | "extract" | "multiExtract";
 export const createRequest = async (
-  input:{
-    "jobType":jobTypeArg, 
-    "jobRunID":string, 
-    "data":{matchIndex:string}
-  }, 
-  callback:(
-    callbackData:callbackType
+  input: {
+    "jobType": jobTypeArg,
+    "jobRunID": string,
+    "data": { matchIndex: string }
+  },
+  callback: (
+    callbackData: callbackType
   ) => void) => {
-  let receivedData:boolean=false;
-  const jobType:string = input["jobType"];
-  let matchIndex:string;
-  let validator =  new Validator(input, CustomParams[input["jobType"]]); 
-  if(jobType == "extract") {
-    matchIndex = validator.validated.data.matchIndex; 
-  }  
+  let receivedData: boolean = false;
+  const jobType: string = input["jobType"];
+  let matchIndex: string;
+  let validator = new Validator(input, CustomParams[input["jobType"]]);
+  if (jobType == "extract") {
+    matchIndex = validator.validated.data.matchIndex;
+  }
 
-  
-  const jobRunID:string = validator.validated.id ;
+
+  const jobRunID: string = validator.validated.id;
   let url = validator.validated.data.url;
   const regexp = validator.validated.data.regexp;
 
-  let response= {
-    data:""
+  let response = {
+    data: ""
   };
-  
-  if(/^ipfs/.test(url)) {
+
+  if (/^ipfs/.test(url)) {
     const ipfsUrlMatch = url.match(/:\/\/(.*)/)
     url = `https://cloudflare-ipfs.com/ipfs/${ipfsUrlMatch[1]}`;
   }
-    await Requester.request(url, customError)
-      .then(responseHttp => {
-        response.data = responseHttp.data;
-        receivedData=true;
-      })
-      .catch(error => {
-        callback({jobRunID, data: { requestStatus:errorStatus.connectError, result:defaultArgs[jobType as keyof defaultArgsTypes] }} )
-      })
 
-   
-  
+  await Requester.request(url, customError)
+    .then(responseHttp => {
+      response.data = responseHttp.data;
+      receivedData = true;
+    })
+    .catch(error => {
+      callback({ jobRunID, data: { requestStatus: errorStatus.connectError, result: defaultArgs[jobType as keyof defaultArgsTypes] } })
+    })
 
 
-console.log('jobType - ' + jobType)
+  if (typeof response.data == "object") {
+    response.data = JSON.stringify(response.data);
+  }
 
-
-  let tempResArray:string[]=[];
-      let resArray:string[]=[];
-      let parseRegExp:string[][];
-      let clearRegExp:string;
-      let flags:string;
-      let re: RegExp=/./;
-      let regexpOK:boolean=false;
-      try {
-      parseRegExp = Array.from(regexp.matchAll(/\/(.*)\/(.*)$/g));
-      clearRegExp = parseRegExp[0][1];
-      flags = parseRegExp[0][2];
-      re = new RegExp(clearRegExp,flags)
-      regexpOK = true;
-      }
-      catch(error) {
-        callback({ jobRunID, data: { requestStatus:errorStatus.regexpError, result:defaultArgs[jobType as keyof defaultArgsTypes] } } )
-      }
-      if(regexpOK && receivedData) {
-        if(jobType == "extract" || jobType=="multiExtract") {
-        
-        const found = response.data.matchAll(re)
-        console.log('found = ' + JSON.stringify(found))
-        Array.from(found, (res) => tempResArray.push(res[1]));
-        console.log('tempResArray - ' + JSON.stringify(tempResArray))
-        if(jobType=="extract") {
-          console.log('tempResArray.length - ' + tempResArray.length)
-          if(Number(input.data["matchIndex"]) < tempResArray.length) {
-            resArray.push(tempResArray[Number(input.data["matchIndex"])]);
-          }
-        } 
-        else {
-          resArray = tempResArray;
-        
+  let tempResArray: string[] = [];
+  let resArray: string[] = [];
+  let parseRegExp: string[][];
+  let clearRegExp: string;
+  let flags: string;
+  let re: RegExp = /./;
+  let regexpOK: boolean = false;
+  try {
+    parseRegExp = Array.from(regexp.matchAll(/\/(.*)\/(.*)$/g));
+    clearRegExp = parseRegExp[0][1];
+    flags = parseRegExp[0][2];
+    re = new RegExp(clearRegExp, flags)
+    regexpOK = true;
+  }
+  catch (error) {
+    callback({ jobRunID, data: { requestStatus: errorStatus.regexpError, result: defaultArgs[jobType as keyof defaultArgsTypes] } })
+  }
+  if (regexpOK && receivedData) {
+    if (jobType == "extract" || jobType == "multiExtract") {
+      const found = response.data.matchAll(re)
+      Array.from(found, (res) => tempResArray.push(res[1]));
+      if (jobType == "extract") {
+        if (Number(input.data["matchIndex"]) < tempResArray.length) {
+          resArray.push(tempResArray[Number(input.data["matchIndex"])]);
         }
-        console.log('resArray' + JSON.stringify(resArray))
-
-        callback({ jobRunID, data: { requestStatus:0, result:resArray } } )
-    } 
-    else if(jobType=="match") {
-      callback({ jobRunID, data: { requestStatus:0, result:re.test(response.data) } } )
       }
+      else {
+        resArray = tempResArray;
+
+      }
+
+      callback({ jobRunID, data: { requestStatus: 0, result: resArray } })
+    }
+    else if (jobType == "match") {
+      callback({ jobRunID, data: { requestStatus: 0, result: re.test(response.data) } })
     }
   }
-    
+}
+
